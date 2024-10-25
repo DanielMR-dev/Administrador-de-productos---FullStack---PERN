40d0718bcff899114a012526e26cb2ac4273941c
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-25 13:57:55 -0500

    361. Colocando la conexion a la BD en variables de Entorno

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize('postgresql://rest_api_node_typescript_dz9f_user:vtYLIZwVJTvlslUhlZp8hpZL0gnD4LwU@dpg-csd9qa3v2p9s7382f4v0-a.oregon-postgres.render.com/rest_api_node_typescript_dz9f?ssl=true');
+const db = new Sequelize(process.env.DATABASE_URL);
 
 export default db;
