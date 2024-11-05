ac03ff35fcb0a9a6bde7bb660cab0939ef14f997
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-05 08:17:08 -0500

    380. Probando Validacion de Crear Producto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,18 @@ import request from 'supertest';
 import server from '../../server';
 
 describe('POST /api/products', () => {
+    test('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({});
+        // Lo que se espera
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(4);
+
+        // Lo que NO se espera
+        expect(response.status).not.toBe(404);
+        expect(response.body.errors).toHaveLength(2);   
+    });
+
     test('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name : "Mouse - Testing",
@@ -16,6 +28,6 @@ describe('POST /api/products', () => {
         expect(response.status).not.toBe(200);
         expect(response.status).not.toBe(400);
         expect(response.status).not.toBe(404);
-        expect(response.body).not.toHaveProperty('error');
+        expect(response.body).not.toHaveProperty('errors');
     });
 });
