50890e82570ca5547b86f4c8d30448bbd11cdf20
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-07 17:03:36 -0500

    385. Probando la actualizacion de Productos

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -79,7 +79,7 @@ describe('GET /api/products', () => {
     });
 });
 
-describe('GET  /api/products', () => {
+describe('GET  /api/products/:id', () => {
     test('should return a 404 response for a non-existent product', async () => {
         const productId = 2000;
         const response = await request(server).get(`/api/products/${productId}`);
@@ -100,4 +100,82 @@ describe('GET  /api/products', () => {
         expect(response.body).toHaveProperty('data');
     });
 });
+
+describe('PUT /api/products/:id', () => {
+    test('should check a valid ID in the URL', async () => {
+        const response = await request(server)
+                                    .put('/api/products/not-valid-url')
+                                    .send({
+                                        name : "Tablet",
+                                        availability : true,
+                                        price: 300
+                                    });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);   
+        expect(response.body.errors[0].msg).toBe('ID no válido');
+    });
+    test('should display validation error messages when updating a product', async () => {
+        const response = await request(server).put('/api/products/1').send({});
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(5);
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+    });
+    test('should validate that the price is greater than 0', async () => {
+        const response = await request(server)
+                                    .put('/api/products/1')
+                                    .send({
+                                        name : "Tablet",
+                                        availability : true,
+                                        price: 0
+                                    });
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe('Precio no válido');
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+    });
+    test('should return a 404 response for a non-existent product', async () => {
+        const productId = 2000;
+        const response = await request(server)
+                                    .put(`/api/products/${productId}`)
+                                    .send({
+                                        name : "Tablet",
+                                        availability : true,
+                                        price: 300
+                                    });
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe('Producto No Encontrado');
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty('data');
+    });
+    test('should update an existing product with valid data', async () => {
+        const response = await request(server)
+                                    .put(`/api/products/1`)
+                                    .send({
+                                        name : "Tablet",
+                                        availability : true,
+                                        price: 300
+                                    });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('data');
+
+        expect(response.status).not.toBe(400);
+        expect(response.body).not.toHaveProperty('errors');  
+    });
+    
+});
+
 
