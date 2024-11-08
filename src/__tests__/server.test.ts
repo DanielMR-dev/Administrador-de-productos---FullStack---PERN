7af0955fd585490166738ed4036c9ba861cd9af4
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-08 16:06:06 -0500

    389. Forzar errores para probar tus catchs

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
-import server  from "../server";
+import server, { connectDB }  from "../server";
+import db from "../config/db";
 
 describe('GET /api', () => {
     test('should send back a JSON response', async () => {
@@ -13,5 +14,21 @@ describe('GET /api', () => {
         // Lo que NO se espera
         expect(res.status).not.toBe(404);
         expect(res.body.msg).not.toBe('desde api');
+    });
+});
+
+jest.mock('../config/db'); // Creamos el mock para simular la conexión a la DB
+
+describe('connectDB', () => {
+    test('should handle database connection error', async () => {
+        jest.spyOn(db, 'authenticate') // Se crea un "espía" que va a esperar que se ejecute db.authenticate
+            .mockRejectedValueOnce(new Error('Hubo un error al conectar a la DB')); // Se fuerza el catch o la exepción
+        const consoleSpy = jest.spyOn(console, 'log'); // Se crea otro "espía" esperando una respuesta en consola
+
+        await connectDB();
+
+        expect(consoleSpy).toHaveBeenCalledWith( // Se espera que el "espía" de la consola contenga un string con el texto especificado
+            expect.stringContaining('Hubo un error al conectar a la DB')
+        );
     });
 });
