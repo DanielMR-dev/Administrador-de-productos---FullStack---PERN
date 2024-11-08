25fc64a668b8033c30956e50a6da07bfe0d72fba
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-08 15:43:49 -0500

    387. Metricas de validacion - Code Coverage

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,37 +2,27 @@ import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['price', 'DESC']
-            ],
-            attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
-        });
-        res.json({data: products});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    const products = await Product.findAll({
+        order: [
+            ['price', 'DESC']
+        ],
+        attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
+    });
+    res.json({data: products});
 };
 
 export const getProductById = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
 
-        if(!product) {
-            res.status(404).json({
-                error: 'Producto No Encontrado'
-            });
-            return;
-        };
+    if(!product) {
+        res.status(404).json({
+            error: 'Producto No Encontrado'
+        });
+        return;
+    };
 
-        res.json({data: product});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    res.json({data: product});
 };
 
 export const createProduct = async (req : Request, res : Response) => {
@@ -44,78 +34,56 @@ export const createProduct = async (req : Request, res : Response) => {
     // const product = await Product.create(req.body);
     // res.json({data: product});
 
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({ data: product });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    const product = await Product.create(req.body);
+    res.status(201).json({ data: product });
 };
 
 export const updateProduct = async (req : Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
 
-        if(!product) {
-            res.status(404).json({
-                error: 'Producto No Encontrado'
-            });
-            return;
-        };
+    if(!product) {
+        res.status(404).json({
+            error: 'Producto No Encontrado'
+        });
+        return;
+    };
 
-        // Actualizar
-        await product.update(req.body); // Producto actualizado
-        await product.save(); // Producto guardado
-
-        res.json({data: product});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
-    
+    // Actualizar
+    await product.update(req.body); // Producto actualizado
+    await product.save(); // Producto guardado
+    res.json({data: product});
 };
+
 export const updateAvailability = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
-    
-        if(!product) {
-            res.status(404).json({
-                error: 'Producto No Encontrado'
-            });
-            return;
-        };
-    
-        // Actualizar
-        product.availability = !product.dataValues.availability;
-        await product.save(); // Producto guardado
-    
-        res.json({data: product});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+
+    if(!product) {
+        res.status(404).json({
+            error: 'Producto No Encontrado'
+        });
+        return;
+    };
+
+    // Actualizar
+    product.availability = !product.dataValues.availability;
+    await product.save(); // Producto guardado
+    res.json({data: product});
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
 
-        if(!product) {
-            res.status(404).json({
-                error: 'Producto No Encontrado'
-            });
-            return;
-        };
+    if(!product) {
+        res.status(404).json({
+            error: 'Producto No Encontrado'
+        });
+        return;
+    };
 
-        // Eliminar
-        await product.destroy();
-        res.json({data: 'Producto Eliminado'});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    // Eliminar
+    await product.destroy();
+    res.json({data: 'Producto Eliminado'});
 };
