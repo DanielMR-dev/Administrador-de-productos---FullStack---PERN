d3fed29cfcbc6def0ddc056aab06b688828c68cf
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-09 11:30:29 -0500

    391. Instalando Swagger

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,21 +1,6 @@
-import request from "supertest";
-import server, { connectDB }  from "../server";
+import { connectDB }  from "../server";
 import db from "../config/db";
 
-describe('GET /api', () => {
-    test('should send back a JSON response', async () => {
-        const res = await request(server).get('/api');
-
-        // Lo que se espera
-        expect(res.status).toBe(200);
-        expect(res.headers['content-type']).toMatch(/json/);
-        expect(res.body.msg).toBe('Desde API');
-
-        // Lo que NO se espera
-        expect(res.status).not.toBe(404);
-        expect(res.body.msg).not.toBe('desde api');
-    });
-});
 
 jest.mock('../config/db'); // Creamos el mock para simular la conexión a la DB
 
