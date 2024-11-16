dfa3dfc866bfe968af9167b94f96f3bf4a4d38be
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-11-16 16:51:15 -0500

    418. Agregando boton para editar

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom";
 import { Product } from "../types";
 import { formatCurrency } from "../utils";
 
@@ -23,7 +24,10 @@ export default function ProductDetails({product} : ProductDetailsProps) {
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
-                    <button>Editar</button>
+                    <Link
+                        to={`products/${product.id}/edit`}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                    >Editar</Link>
                 </div>
             </td>
         </tr> 
